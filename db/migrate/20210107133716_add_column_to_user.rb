ca55e2aa8c7f5f86144312d83a19162c15b34f5b
terachan3700 <chapico01@gmail.com>
2021-01-08 08:45:52 +0900

    userのマイグレーションファイルを追加

---
+++ db/migrate/20210107133716_add_column_to_user.rb
@@ -0,0 +1,13 @@
+class AddColumnToUser < ActiveRecord::Migration[6.1]
+  def up
+    add_column :users, :zip_code, :integer
+    add_column :users, :address, :string
+    add_column :users, :self_introduction, :text
+  end
+
+  def down
+    remove_column :users, :zip_code, :integer
+    remove_column :users, :address, :string
+    remove_column :users, :self_introduction, :text
+  end
+end
