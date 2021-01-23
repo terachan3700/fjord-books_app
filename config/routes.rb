e973f41478a55d1161002d228e5c648ed189a038
terachan3700 <chapico01@gmail.com>
2021-01-23 21:16:48 +0900

    ユーザー一覧とユーザー詳細画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
       :registrations => 'users/registrations',
       :sessions => 'users/sessions'
   }
+  resources :users, only: [:index, :show]
 
   devise_scope :user do
     get "sign_in", :to => "users/sessions#new"
