4b53e3fca5b758c1a26279c33a05f15f247b2b89
terachan3700 <chapico01@gmail.com>
2021-01-24 00:30:49 +0900

    郵便番号のバリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-  validates :zip_code, format: { with: /\A\d{3}-\d{4}\z/ }
+  validates :zip_code, format: { with: /\A\d{3}-\d{4}\z/ }, if: :is_zip_code?
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private
+
+  def is_zip_code?
+    zip_code.present?
+  end
 end
