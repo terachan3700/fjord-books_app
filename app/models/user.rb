9d7dbba3937e4140dde368822cbccb6529b34d4e
terachan3700 <chapico01@gmail.com>
2021-01-24 10:24:58 +0900

    deviseのdefaultのコメントを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@
 
 class User < ApplicationRecord
   validates :zip_code, format: { with: /\A\d{3}-\d{4}\z/ }, if: :zip_code?
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
