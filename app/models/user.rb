347e27e391fdad9fbcee268aef38ed532eecf731
terachan3700 <chapico01@gmail.com>
2021-01-24 18:23:03 +0900

    oauth登録の場合、ランダムなパスワードを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.name = auth.info.name
+      user.password = Devise.friendly_token[0, 20]
       user.provider = auth[:provider]
       user.uid = auth[:uid]
     end
