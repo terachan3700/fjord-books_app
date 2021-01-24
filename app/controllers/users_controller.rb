bb0160ed3652f68402f0909007beec7add8be4f8
terachan3700 <chapico01@gmail.com>
2021-01-24 10:35:53 +0900

    ページネーションを検証しやすいように5にする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,10 @@
 
 class UsersController < ApplicationController
   def index
-    @users = User.order(:id).page(params[:page])
+    @users = User.order(:id).page(params[:page]).per(5)
   end
 
   def show
     @user = User.find(params[:id])
-    @login_user = true if current_user.id == @user.id
   end
 end
