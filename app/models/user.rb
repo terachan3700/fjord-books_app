28cb833b515dc07e3c62c4fe45b23fb0a9e318a6
terachan3700 <chapico01@gmail.com>
2021-01-24 09:59:33 +0900

    rubocopを適用

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :zip_code, format: { with: /\A\d{3}-\d{4}\z/ }, if: :is_zip_code?
+  validates :zip_code, format: { with: /\A\d{3}-\d{4}\z/ }, if: :zip_code?
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
   private
 
-  def is_zip_code?
+  def zip_code?
     zip_code.present?
   end
 end
--- db/migrate/20210107133716_add_column_to_user.rb
+++ db/migrate/20210107133716_add_column_to_user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddColumnToUser < ActiveRecord::Migration[6.1]
   def up
     add_column :users, :zip_code, :integer
